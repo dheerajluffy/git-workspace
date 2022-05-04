c63b46e1ad0ce6ceb821413eb11b5b65893f604f
dheerajluffy <dheerajluffy@gmail.com>
2022-05-04 10:30:18 +0530

    added smartControlls

---
+++ smartControlls/media.py
@@ -0,0 +1,15 @@
+import cv2
+import mediapipe as mp 
+import time
+
+cap =cv2.VideoCapture(0)
+
+mpHands=mp.solutions.mediapipe.python.solutions.hands
+hands =mpHands.Hands()
+
+while True:
+    success , img = cap.read()
+    imgGray=cv2.cvtColor(img,cv2.COLOR_BAYER_BG2GRAY)
+    results = hands.process(imgGray)
+    cv2.imshow("Image", img)
+    cv2.waitkey(1)
